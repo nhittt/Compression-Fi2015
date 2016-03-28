7a390e35706db0bf227b4918489ae16cc5194d99
nhittt <thaonhitt2005@gmail.com>
2016-03-28 15:04:18 +0700

    done top best compression

--- topkbest.py
+++ topkbest.py
@@ -10,79 +10,119 @@ import io
 from nltk import MaxentClassifier
 from itertools import *
 
+
 def powerset(iterable):
 	"powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
 	s = list(iterable)
-	return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
+	return chain.from_iterable(combinations(s, r) for r in range(len(s) + 1))
+
 
 def prob_deletion_1_edge(json_object, maxent_model, edge_n_m):
-	context_option=1
-	featureset=get_context_1_edge(json_object, edge_n_m , context_option)
-	return maxent_model.prob_classify(featureset)
+	context_option = 1
+	featureset = get_context_1_edge(json_object, edge_n_m, context_option)
+	return maxent_model.prob_classify(featureset).prob(0)
+
 
 def prob_deletion_sentence(json_object, maxent_model):
-	context_option=1
-	featuresets=get_context_sentence(json_object, context_option)
-	return maxent_model.prob_classify_many(featuresets)
+	context_option = 1
+	featuresets = get_context_sentence(json_object, context_option)
+	return maxent_model.prob_classify_many(featuresets).prob(0)
+
 
 def search_children(json_object, node_n):
-	children_list=[]
+	children_list = []
 	edges = get_edges(json_object)
 	for edge in edges:
 		(parent_id, child_id, label) = get_elements_in_edge(edge)
-		if parent_id==node_n:
+		if parent_id == node_n:
 			children_list.append(child_id)
 	return children_list
 
+
 def search_highest_child_root(json_object, maxent_model):
 	edges = get_edges(json_object)
-	children_root=[]
+	children_root = []
 	for edge in edges:
 		(parent_id, child_id, label) = get_elements_in_edge(edge)
-		if parent_id ==-1:
-			children_root=search_children(json_object, parent_id)
+		if parent_id == -1:
+			children_root = search_children(json_object, parent_id)
 			break
-	highest_score_child_root= max([prob_deletion_1_edge(json_object, maxent_model, (-1,child)) for child in children_root])
-	return highest_score_child_root
-		
+	highest_score_child_root = prob_deletion_1_edge(json_object, maxent_model, (-1, children_root[0]))
+	highest_child_root = children_root[0]
+	for i in range(1, len(children_root)):
+		if prob_deletion_1_edge(json_object, maxent_model, (-1, children_root[i])) > highest_score_child_root:
+			highest_score_child_root = prob_deletion_1_edge(json_object, maxent_model, (-1, children_root[i]))
+			highest_child_root = children_root[i]
+	return highest_child_root
 
-#top scoring compression
-	
+
+# top scoring compression
+
 def max_subset(json_object, maxent_model, node_n):
-	children_n=search_children(json_object, node_n)
-	subset_children=list(powerset(children_n))
-	max_score_C_n=0
-	max_subset=()
+	children_n = search_children(json_object, node_n)
+	subset_children = list(powerset(children_n))
+	prob_max = 0
+	for child in children_n:
+		prob_max += 1 - prob_deletion_1_edge(json_object, maxent_model, (node_n, child))
+	max_score_C_n = prob_max
+	max_subset = tuple(children_n)
+
 	for subset in subset_children:
-		score_C_n=0
+		score_C_n = 0
 		for child in children_n:
-			prob=prob_deletion_1_edge(json_object, maxent_model, (node_n, subset))
-			if child not in subset:
-				prob=1-prob
-			score_C_n+=prob
-		if max_score_C_n>score_C_n:
-			max_score_C_n=score_C_n
-			max_subset=subset
+			prob = prob_deletion_1_edge(json_object, maxent_model, (node_n, child))
+			if child in subset:
+				prob = 1 - prob
+			score_C_n += prob
+		if max_score_C_n < score_C_n:
+			max_score_C_n = score_C_n
+			max_subset = subset
 	return max_subset
+
+
+max_subsets = []
+
 
 def max_subset_sentence(json_object, maxent_model, node_n):
 	if not search_children(json_object, node_n):
-		return node_n
+		global max_subsets
+		max_subsets = []
+		max_subsets.append(node_n)
 	else:
 		for child in max_subset(json_object, maxent_model, node_n):
 			max_subset_sentence(json_object, maxent_model, child)
+			max_subsets.extend(max_subset(json_object, maxent_model, node_n))
+	return max_subsets
 
 
 def top_scoring_compression(json_object, maxent_model):
-	top_best_compress_edges=[]
+	top_best_compress_edges = []
 	node_n = search_highest_child_root(json_object, maxent_model)
+	top_best_compress_edges.append(node_n)
 	top_best_compress_edges.extend(max_subset_sentence(json_object, maxent_model, node_n))
 	return top_best_compress_edges
-			
-		
+
+
+def top_scoring_compression_sentence(json_object, maxent_model):
+	nodes = get_nodes(json_object)
+	words_form = []
+	id_form = {}
+	for node in nodes:
+		word_in_form = get_words_in_node(node)
+		words_form.append(word_in_form)
+		for item in get_words_id_form_tag(word_in_form):
+			id_form[item[0]] = item[1]
+	top_best_compress = sorted(top_scoring_compression(json_object, maxent_model))
+	top_best_compress_edges = []
+	for edge in top_best_compress:
+		if edge not in top_best_compress_edges:
+			top_best_compress_edges.append(edge)
+	return top_best_compress_edges
+
 
 if __name__ == '__main__':
 	with open('model/compress_model_0123_test', 'rb') as fmodel:
 		maxent = pickle.load(fmodel)
-	#maxent.show_most_informative_features()
-	print top_scoring_compression(read_jsonfile('testdata.json'),maxent)
+	# maxent.show_most_informative_features()
+	for json_object in read_jsonfile('data/compression_data_0.json'):
+		print top_scoring_compression_sentence(json_object, maxent)
--- training.py
+++ training.py
@@ -11,122 +11,122 @@ from nltk import MaxentClassifier
 
 # read json input
 def read_jsonfile(json_file):
-	objects = []
-	data = ''
-	with io.open(json_file, 'r', encoding='utf8') as f:
-		for line in f:
-			if line in ['\n', '\n\r']:
-				objects.append(json.loads(data))
-				data = ''
-			else:
-				data += line
-		try:
-			objects.append(json.loads(data))
-		except:
-			return objects
-	return objects
+    objects = []
+    data = ''
+    with io.open(json_file, 'r', encoding='utf8') as f:
+        for line in f:
+            if line in ['\n', '\n\r']:
+                objects.append(json.loads(data))
+                data = ''
+            else:
+                data += line
+        try:
+            objects.append(json.loads(data))
+        except:
+            return objects
+    return objects
 
 
 def read_tiny_jsonfile(json_file):
-	with open(json_file) as f:
-		data = json.load(f)
-		return data
+    with open(json_file) as f:
+        data = json.load(f)
+        return data
 
 
 # get sentence(id,sentence,nodes, edges, NE)
 def get_idsentence(object):
-	return object['graph']['id']
+    return object['graph']['id']
 
 
 def get_sentence(object):
-	return object['graph']['sentence']
+    return object['graph']['sentence']
 
 
 def get_nodes(object):
-	nodes = []
-	for i in range(len(object['graph']['node'])):
-		nodes.append(object['graph']['node'][i])
-	return nodes
+    nodes = []
+    for i in range(len(object['graph']['node'])):
+        nodes.append(object['graph']['node'][i])
+    return nodes
 
 
 def get_edges(object):
-	edges = []
-	for i in range(len(object['graph']['edge'])):
-		edges.append(object['graph']['edge'][i])
-	return edges
+    edges = []
+    for i in range(len(object['graph']['edge'])):
+        edges.append(object['graph']['edge'][i])
+    return edges
 
 
 def get_name_entity(object):
-	name_entity = []
-	for i in range(len(object['graph']['entity_mention'])):
-		name_entity.append(object['graph']['entity_mention'][i])
-	return name_entity
+    name_entity = []
+    for i in range(len(object['graph']['entity_mention'])):
+        name_entity.append(object['graph']['entity_mention'][i])
+    return name_entity
 
 
 # get nodes(form, words, gender, head_word_index) in sentence
 def get_form_in_node(node):
-	return node['form']
+    return node['form']
 
 
 def get_words_in_node(node):
-	return node['word']
+    return node['word']
 
 
 def get_gender_in_node(node):
-	return node['gender']
+    return node['gender']
 
 
 def get_headwordindex_in_node(node):
-	return node['head_word_index']
+    return node['head_word_index']
 
 
 # get words in nodes
 def get_words_id_form_tag(words_in_form):
-	words_from_form = []
-	for i in range(len(words_in_form)):
-		(id, form, stem, tag) = (
-		words_in_form[i]['id'], words_in_form[i]['form'], words_in_form[i]['stem'], words_in_form[i]['tag'])
-		words_from_form.append((id, form, stem, tag))
-	return words_from_form
+    words_from_form = []
+    for i in range(len(words_in_form)):
+        (id, form, stem, tag) = (
+            words_in_form[i]['id'], words_in_form[i]['form'], words_in_form[i]['stem'], words_in_form[i]['tag'])
+        words_from_form.append((id, form, stem, tag))
+    return words_from_form
 
 
 # get edges' elements in sentence
 def get_elements_in_edge(edge):
-	return (edge['parent_id'], edge['child_id'], edge['label'])
+    return (edge['parent_id'], edge['child_id'], edge['label'])
 
 
 # get entity's elements
 def get_elements_in_NE(name_entity):
-	return (name_entity['start'], name_entity['end'], name_entity['head'], name_entity['name'], name_entity['type'],
-			name_entity['is_proper_name_entity'])
+    return (name_entity['start'], name_entity['end'], name_entity['head'], name_entity['name'], name_entity['type'],
+            name_entity['is_proper_name_entity'])
 
 
 # get compression (text, edges)
 def get_compression_text(object):
-	return object['compression']['text']
+    return object['compression']['text']
 
 
 def get_compression_edges(object):
-	return object['compression']['edge']
+    return object['compression']['edge']
 
 
 # get compression's edge element
 def get_compression_edge_element(edge):
-	return (edge['parent_id'], edge['child_id'])
+    return (edge['parent_id'], edge['child_id'])
 
 
 # read input json
 def readfile(filename):
-	with open(filename) as f:
-		for line in f:
-			while line in ['\n', '\r\n']:
-				try:
-					data = json.loads(line)
-				except ValueError:
-					line += next(f)
+    with open(filename) as f:
+        for line in f:
+            while line in ['\n', '\r\n']:
+                try:
+                    data = json.loads(line)
+                except ValueError:
+                    line += next(f)
 
-				id = data['graph']['sentence']
-				print(id)
+                id = data['graph']['sentence']
+                print(id)
 
 
 # get context for 1 edge (json_object, egde,  option_train=0, option_test=1)
@@ -139,6 +139,7 @@ def get_context_1_edge(json_object, edge_n_m, context_option):
 	compress_pa_ch = []
 	id_word = {}
 	id_tag = {}
+	nes_nee_neh_name_type_proper= {}
 	pa_ch_label = {}
 	for node in nodes:
 		word_in_form = get_words_in_node(node)
@@ -151,16 +152,15 @@ def get_context_1_edge(json_object, edge_n_m, context_option):
 		pa_ch_label[(parent_id, child_id)] = label
 	for entity in name_entity:
 		(start, end, head, name, type, is_ne) = get_elements_in_NE(entity)
-		NEs_NEe_Neh_name_type_is[(start, end, head)] = (name, type, is_ne)
+		nes_nee_neh_name_type_proper[(start, end, head)] = (name, type, is_ne)
 	for compress_edge in compress_edges:
 		compress_pa_ch.append(get_compression_edge_element(compress_edge))
-
 	(parent_id, child_id) = edge_n_m
 	if (parent_id, child_id) in compress_pa_ch:
 		compress_tag = 1
 	else:
 		compress_tag = 0
-	co = getContextall(parent_id, child_id, pa_ch_label, id_word, id_tag, NEs_NEe_Neh_name_type_is, words_form)
+	co = getContextall(parent_id, child_id, pa_ch_label, id_word, id_tag, nes_nee_neh_name_type_proper, words_form)
 
 	if context_option == 0:
 		return (co, compress_tag)
@@ -169,33 +169,33 @@ def get_context_1_edge(json_object, edge_n_m, context_option):
 
 
 def get_context_sentence(json_object, context_option):
-	edges = get_edges(json_object)
-	context_sentence = []
-	for edge in edges:
-		(parent_id, child_id, label) = get_elements_in_edge(edge)
-		context_sentence.append(get_context_1_edge(json_object, (parent_id, child_id), context_option))
-	return context_sentence
+    edges = get_edges(json_object)
+    context_sentence = []
+    for edge in edges:
+        (parent_id, child_id, label) = get_elements_in_edge(edge)
+        context_sentence.append(get_context_1_edge(json_object, (parent_id, child_id), context_option))
+    return context_sentence
 
 
 def training(list_filename, model_name):
-	# training in large data
-	context_data = []
-	for filename in list_filename:
-		json_objects = read_jsonfile(filename)
-		for json_object in objects:
-			context_data.extend(get_context_sentence(json_object, 0))
-	print('Done get contexts')
-	m = MaxentClassifier.train(context_data, max_iter=100)
-	with open(model_name, 'wb') as fmodel:
-		pickle.dump(m, fmodel)
-	print('Finish training maxent model')
+    # training in large data
+    context_data = []
+    for filename in list_filename:
+        json_objects = read_jsonfile(filename)
+        for json_object in json_objects:
+            context_data.extend(get_context_sentence(json_object, 0))
+    print('Done get contexts')
+    m = MaxentClassifier.train(context_data, max_iter=100)
+    with open(model_name, 'wb') as fmodel:
+        pickle.dump(m, fmodel)
+    print('Finish training maxent model')
 
 
 if __name__ == "__main__":
-	#test('testdata.json','compress_model')
-	"""list_train=[]
-	for i in range(len(sys.argv)-1):
-		list_train.append('data/compression_data_%s.json'%sys.argv[i+1])
-	print(list_train)
-	training(list_train,'compress_model_0123_100')"""
-	#test('data/compression_data_4.json','compress_model_0123_100','data/output_4_100')
+    # test('testdata.json','compress_model')
+    list_train = []
+    for i in range(len(sys.argv) - 1):
+        list_train.append('data/compression_data_%s.json' % sys.argv[i + 1])
+    print(list_train)
+    training(list_train, 'compress_model_0123_100')
+# test('data/compression_data_4.json','compress_model_0123_100','data/output_4_100')
